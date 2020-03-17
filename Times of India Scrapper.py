49e2b179aa48449f58548e9a54b9c5a5e33ecde4
Wiki626 <60113179+Wiki626@users.noreply.github.com>
2020-03-17 11:33:22 -0400

    Made some minor edits
    The scrapper now tells you what keywords it hit on for each section

--- Times of India Scrapper.py
+++ Times of India Scrapper.py
@@ -6,7 +6,8 @@ import requests
 base_url = 'https://timesofindia.indiatimes.com'
 location = '/world/us'
 
-keywords = ('Fed', 'White House', 'Trump', 'US Senate', 'US Government', 'Supreme Court', 'House of Representatives', 'Congress', 'US President', 'Capitol Hill')
+keywords = ('Fed', 'White House', 'Trump', 'US Senate', 'US Government', 'Supreme Court', 'House of Representatives',
+            'Congress', 'US President', 'Capitol Hill', 'Washington')
 
 # Function for setting up the soup
 def get_soup(base_url, location):
@@ -19,13 +20,13 @@ soup = get_soup(base_url, location)
 
 # Grabs the "top news" stories from the page and iterates down to the "li" tags
 top_news = {}
-top_news_list = soup.find('ul', class_ = 'top-newslist clearfix')
+top_news_list = soup.find('ul', class_='top-newslist clearfix')
 top_news_items = top_news_list.find_all('li')
 
 # Grabs the html location and title for each "top news" story and saves them to a dictionary
 for item in top_news_items:
     for top_news_items in item.find_all('a'):
-        top_news.update({top_news_items.get('href'):top_news_items.get('title')})
+        top_news.update({top_news_items.get('href'): top_news_items.get('title')})
 
 # Cleans the dictionary of "None" type entries
 filtered = {k: v for k, v in top_news.items() if v is not None}
@@ -34,26 +35,30 @@ top_news.update(filtered)
 
 # Grabs the "latest news" stories from the page and iterates down to the "li" tags
 latest_stories = {}
-latest_stories_list = soup.find('ul', class_ = 'list5 clearfix')
+latest_stories_list = soup.find('ul', class_='list5 clearfix')
 latest_stories_items = latest_stories_list.find_all('li')
 
 # Grabs the html location and title for each "latest news" story and saves them to a dictionary
 for item in latest_stories_items:
     for latest_stories_items in item.find_all('a'):
-        latest_stories.update({latest_stories_items.get('href'):latest_stories_items.get('title')})
+        latest_stories.update({latest_stories_items.get('href'): latest_stories_items.get('title')})
 
 # Cleans the dictionary of "None" type entries
 filtered = {k: v for k, v in latest_stories.items() if v is not None}
 latest_stories.clear()
 latest_stories.update(filtered)
 
-# Definition for filtering the values of dictionaries by keywords and return keys to a list (only if the key has not already been added to that list)
+# Definition for filtering the values of dictionaries by keywords and return keys to a list (only if the key has not
+# already been added to that list)
 def dict_search(input_dict, keywords_list, save_location):
+    temp = []
     for k, v in input_dict.items():
         for keyword in keywords_list:
             if keyword in v:
                 if v not in save_location:
                     save_location.append(k)
+                    temp.append(keyword)
+    print(f'The keywords that triggered were {temp}.')
 
 
 top_news_links = []
@@ -61,10 +66,9 @@ latest_stories_links = []
 
 # Pulling the html location keys out of the dictionaries created above into a pair of lists
 dict_search(top_news, keywords, top_news_links)
+print(f'{len(top_news_links)} results where found in Times of India US Top News Stories.')
 dict_search(latest_stories, keywords, latest_stories_links)
-
-print(f'{len(top_news_links)} results where found in Top News Stories.')
-print(f'{len(latest_stories_links)} results where found in Latest News Stories.')
+print(f'{len(latest_stories_links)} results where found in Times of India US Latest News Stories.')
 
 article_text = ''
 
@@ -75,6 +79,7 @@ def article_lookup(links, base_url):
         temp = soup.find(class_='_3WlLe clearfix')
         global article_text
         article_text = article_text + ' ' + temp.get_text()
+
 
 # Pulling the articles and saving their text to a string
 article_lookup(top_news_links, base_url)
